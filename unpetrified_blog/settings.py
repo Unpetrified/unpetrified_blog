9fdbc731b891e15c05d0b4c9257ad6b290938dbd
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2022-09-25 04:44:48 +0100

    Update settings.py

--- unpetrified_blog/settings.py
+++ unpetrified_blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2#6&#c3ujz*sfq&)&i-1*rz1mp^2u*_!6d5o2ta9&#69cnc(kh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['unpetrified', '127.0.0.1']
+ALLOWED_HOSTS = ['unpetrified.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
