17b8a3697370169a98c2cc50ed4ee3835aa39324
Kings <kingsleyobiefuna01@gmail.com>
2022-10-01 00:37:07 +0100

    tried to fix the ui on ios

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
+from re import template
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
     path('post/<int:post_id>', views.Posts.as_view(), name='post'),
     path('register', views.Register.as_view(), name='register'),
     path('create/post', views.CreatePost.as_view(), name='create_post'),
+    path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('password/reset', auth_views.PasswordResetView.as_view(template_name='reset/reset.html'), name='reset_password'),
     path('<int:post_id>', views.Comment.as_view(), name='comment'),
 ]
--- unpetrified_blog/urls.py
+++ unpetrified_blog/urls.py
@@ -23,6 +23,5 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root' : settings.MEDIA_ROOT}),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
